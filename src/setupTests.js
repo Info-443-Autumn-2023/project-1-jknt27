2478d35179931d17f753d5f2972b0588aec77d97
Alexander Bell-Towne <alexander@bell-towne.com>
2023-10-24 09:54:26 -0700

    Add signout to firebase mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -238,6 +238,7 @@ jest.mock('firebase/auth', () => {
     getAuth: () => {
       return auth;
     },
+    signOut: async () => {},
     onAuthStateChanged: (auth, callback) => {
       callback(MOCK_USER); //"log in" the mock user
       return () => { } //off
